d943ecbf0bfef21fe6f688013f2595ea5dd97d88
Jobzi <jipsonmurillo@gmail.com>
2021-09-13 13:11:49 -0500

    CRUD with API

--- main.go
+++ main.go
@@ -1,35 +1,135 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
-type UserSchema struct {
-	id       string `json:"id"`
-	user     string `json:"user"`
-	password string `json:"user"`
+// Types
+type taskSchema struct {
+	ID      int    `json:"ID"`
+	Name    string `json:"Name"`
+	Content string `json:"Content"`
 }
 
-type Users []UserSchema
+type allTasks []taskSchema
 
-var users = Users{
+// Persistence
+var tasks = allTasks{
 	{
-		id:       "Qwerty",
-		user:     "hola@gmail.com",
-		password: "Hola",
+		ID:      1,
+		Name:    "Task One",
+		Content: "Some Content",
 	},
 }
 
 func IndexRouter(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, users)
+	fmt.Fprintf(w, "<h1>WELCOME TO MY API</h1>")
+}
+
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func createTasks(w http.ResponseWriter, r *http.Request) {
+	var newTask taskSchema
+	reqBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		fmt.Fprintf(w, "INSER VALID TASK")
+	}
+
+	json.Unmarshal(reqBody, &newTask)
+	newTask.ID = len(tasks) + 1
+	tasks = append(tasks, newTask)
+
+	w.Header().Set("Content-Type", "application/json") //cabecera
+	w.WriteHeader(http.StatusCreated)                  //Si el tipo de dato fue correcto
+	json.NewEncoder(w).Encode(newTask)                 //contendido
+}
+
+func getTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)["id"]
+
+	taskId, err := strconv.Atoi(vars)
+
+	if err != nil {
+		fmt.Fprintf(w, "Invalid ID")
+		return
+	}
+
+	for _, task := range tasks {
+		if task.ID == taskId {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(task)
+		}
+	}
+
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)["id"]
+
+	taskId, err := strconv.Atoi(vars)
+
+	if err != nil {
+		fmt.Fprintf(w, "Invalid ID")
+		return
+	}
+
+	for index, task := range tasks {
+		if task.ID == taskId {
+			tasks = append(tasks[:index], tasks[index+1:]...)
+			fmt.Fprintf(w, "The task with ID %v has been remove successfully", taskId)
+		}
+	}
+
+}
+
+func updateTask(w http.ResponseWriter, r *http.Request) {
+	var updatedTask taskSchema
+
+	vars := mux.Vars(r)["id"]
+
+	taskId, err := strconv.Atoi(vars)
+
+	if err != nil {
+		fmt.Fprintf(w, "Invalid ID")
+		return
+	}
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Please Enter Valid Data")
+	}
+	json.Unmarshal(reqBody, &updatedTask)
+
+	for i, t := range tasks {
+		if t.ID == taskId {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+
+			updatedTask.ID = t.ID
+			tasks = append(tasks, updatedTask)
+			fmt.Fprintf(w, "The task with ID %v has been updated successfully", taskId)
+		}
+	}
 }
 
 func main() {
-	r := mux.NewRouter()
+	r := mux.NewRouter().StrictSlash(true)
 	r.HandleFunc("/", IndexRouter)
+	r.HandleFunc("/task", getTasks).Methods("GET")
+	r.HandleFunc("/task", createTasks).Methods("POST")
+	r.HandleFunc("/task/{id}", getTask).Methods("GET")
+	r.HandleFunc("/task/{id}", deleteTask).Methods("DELETE")
+	r.HandleFunc("/task/{id}", updateTask).Methods("PUT")
 
-	http.ListenAndServe(":3005", r)
+	log.Fatal(http.ListenAndServe(":3005", r))
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 # API REST WITH MONGODB
 * go run main.go
+* go get github.com/githubnemo/CompileDaemon //sierve para que se complie como el nodemon en js
